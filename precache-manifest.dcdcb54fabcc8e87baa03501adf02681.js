c7ab5f3cec2ea7923b11646debb537e735a84d60
ivancho277 <rouskoi@live.com>
2019-04-30 09:09:50 -0700

    Updates

---
+++ precache-manifest.dcdcb54fabcc8e87baa03501adf02681.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9176cdfeda3ae090161e86afda685b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "96a63c9918d79ec6b68c",
+    "url": "/clicky-game/static/css/main.34542f26.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "96a63c9918d79ec6b68c",
+    "url": "/clicky-game/static/js/main.207d7f46.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "64151d333820baab0f6131329dadc215",
+    "url": "/clicky-game/static/media/nibbler.64151d33.jpg"
+  },
+  {
+    "revision": "f6fb540170cfece28c6d98408872aa33",
+    "url": "/clicky-game/static/media/robot-santa.f6fb5401.jpg"
+  }
+]);
